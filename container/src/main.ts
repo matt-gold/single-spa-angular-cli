1dd57af5a006909548195813254b260951bad610
Matt Gold <mattbgold@gmail.com>
2020-11-13 13:26:57 -0500

    remove invocation of deprecated single-spa-angular internal function

--- container/src/main.ts
+++ container/src/main.ts
@@ -5,7 +5,6 @@ import { getSingleSpaExtraProviders } from 'single-spa-angular';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { PlatformLocation } from '@angular/common';
 
 if (environment.production) {
   enableProdMode();
@@ -23,11 +22,9 @@ platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule)
       return window.Zone.current._properties[appId] === true;
     };
 
-    const rootPlatformLocation = module.injector.get(PlatformLocation) as any;
     const rootZone = module.injector.get(NgZone);
 
     // tslint:disable-next-line:no-string-literal
     rootZone['_inner']._properties[appId] = true;
-    rootPlatformLocation.setNgZone(rootZone);
   })
   .catch(err => console.error(err));
